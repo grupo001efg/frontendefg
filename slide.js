39200fd68986f287a4614ac91a067eca1e26e628
grupo001efg <163312970+grupo001efg@users.noreply.github.com>
2024-08-23 10:42:20 -0300

    versao.0.0.5 slide show

---
+++ slide.js
@@ -0,0 +1,24 @@
+var slide = document.querySelector('.slideshow');
+var imagens = [
+    './imagem/01.jpg',
+    './imagem/02.jpg',
+    './imagem/03.jpg',
+    './imagem/04.jpg'
+];
+
+var tempo = 3000;
+var cont = 0;
+
+function moverSlide(){
+    slide.src = imagens[cont];
+
+    if (cont < imagens.length - 1){
+        cont++;
+    }else{
+        cont = 0;
+    }
+
+    setTimeout(moverSlide(), tempo);
+}
+
+window.onload = moverSlide;
